50bf08772a85110dd556cbb4c85498b783be7a55
K <luca@pretto.eu>
2019-01-22 17:49:01 +0100

    aggiunti dati termini pagamento

---
+++ l10n_it_payment_term_riba_data/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+{
+    'name': 'Termini di pagamento per ricevute bancarie',
+    'version': '10.0.1.0',
+    'category': 'Localisation/Italy',
+    'description': """Ri.ba. sbf data.""",
+    'author': 'SC & LP',
+    'license': 'AGPL-3',
+    "depends": [
+        'l10n_it_ricevute_bancarie',
+    ],
+    "data": [
+        'data/payment_data.xml',
+    ],
+    'installable': True
+}
