6920a35a460ca57d5350fea05c280490d758ec34
K <luca@pretto.eu>
2019-02-08 21:21:08 +0100

    [UPD] l10n_it_fatturapa_out_fiscal_note

--- l10n_it_fatturapa_out_fiscal_note/wizard/wizard_export_fatturapa.py
+++ l10n_it_fatturapa_out_fiscal_note/wizard/wizard_export_fatturapa.py
@@ -9,5 +9,19 @@ class WizardExportFatturapa(models.TransientModel):
     def setDatiGeneraliDocumento(self, invoice, body):
         res = super(WizardExportFatturapa, self).setDatiGeneraliDocumento(invoice, body)
         if invoice.fiscal_position_id.note:
-            body.DatiGenerali.DatiGeneraliDocumento.Causale = invoice.fiscal_position_id.note
+            # max length of Causale is 200
+            caus_list = invoice.fiscal_position_id.note.split('\n')
+            for causale in caus_list:
+                if not causale:
+                    continue
+                causale_list_200 = \
+                    [causale[i:i + 200] for i in range(0, len(causale), 200)]
+                for causale200 in causale_list_200:
+                    # Remove non latin chars, but go back to unicode string,
+                    # as expected by String200LatinType
+                    causale = causale200.encode(
+                        'latin', 'ignore').decode('latin')
+                    body.DatiGenerali.DatiGeneraliDocumento.Causale \
+                        .append(causale)
         return res
+
