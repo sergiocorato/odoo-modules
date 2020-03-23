0bcde89d20d3a553adc215713e8610d176a628d1
sergiocorato <sergiocorato@gmail.com>
2020-03-23 15:27:10 +0100

    index on 12.0: 3d79e9a [PORT] 12.0

--- l10n_it_payment_term_data/__manifest__.py
+++ l10n_it_payment_term_data/__manifest__.py
@@ -1,18 +1,17 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Italian Localization - Termini di pagamento comuni',
-    'version': '10.0.1.2.1',
+    'version': '11.0.1.0.0',
     'category': 'Localization/Italy',
     'author': 'SC & LP',
     'license': 'AGPL-3',
     'depends': [
-        'account_payment_term_month',
-        'l10n_it_fatturapa'
+        'account_payment_term_extension',
+        'l10n_it_fiscal_payment_term',
     ],
     'data': [
         'data/payment_data.xml',
     ],
-    'installable': False
+    'installable': True,
 }
